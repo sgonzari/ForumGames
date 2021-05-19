48ea21cb22c6554e0fee451bcc966add10073e71
SrCbas <cbastiangr456@gmail.com>
2021-05-20 00:32:34 +0200

    Change button of login to submit

--- Games-FE/js/login.js
+++ Games-FE/js/login.js
@@ -1,25 +1,24 @@
-$(document).ready(function(){
-    $("#go").click(function(){
-        var username = $("#login").val().trim();
-        var passwd = $("#password").val().trim();
+function handleSubmit(event) {
+    event.preventDefault()
+    var username = $("#login").val().trim();
+    var passwd = $("#password").val().trim();
 
-        if( username != "" && password != "" ){
-            $.ajax({
-                url: 'https://localhost:44355/login',
-                dataType: 'json',
-                type: 'post',
-                contentType: 'application/json',
-                data: JSON.stringify({username:username,passwd:passwd}),
-                success: function(data, status){
-                    //alert("Data: " + data + "\nStatus: " + status);
-                    if(data){ 
-                        window.location.replace("./user.html");
-                        window.localStorage.setItem('usuario', username)
-                    } else {
-                        $("#error").css("display","block");
-                    }
-				}
-            })
-        }
-    });
-});
+    if (username != "" && password != "") {
+        $.ajax({
+            url: 'https://localhost:44355/login',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify({ username: username, passwd: passwd }),
+            success: function (data, status) {
+                //alert("Data: " + data + "\nStatus: " + status);
+                if (data) {
+                    window.location.replace("./user.html");
+                    window.localStorage.setItem('usuario', username)
+                } else {
+                    $("#error").css("display", "block");
+                }
+            }
+        })
+    }
+}
