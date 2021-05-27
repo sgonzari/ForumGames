288212d958ad5e2a28778e26438385bde319047c
SrCbas <cbastiangr456@gmail.com>
2021-05-27 02:02:09 +0200

    Added strucutre Comments

---
+++ Games.BL/Implementations/ComentarioBL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Games.BL.Implementations
+{
+    public class ComentarioBL
+    {
+    }
+}
---
+++ Games.Core/DTO/ComentarioDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Games.CORE.DTO
+{
+    public class ComentarioDTO
+    {
+    }
+}
